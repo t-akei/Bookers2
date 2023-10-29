b9d3c3bebc4b3c752cb618e039c375adab940942
t-akei <145541870+t-akei@users.noreply.github.com>
2023-10-29 04:49:57 +0000

    [課題途中]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
 
   def index
-    @user = #User.find(params[:id])
+    @users = User.all
+    @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
-    #@user_image = @user.profile_image
   end
 
   def edit
   end
-  
+
   def update
   end
 
