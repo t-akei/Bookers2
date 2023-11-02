e7a5ebd8d080f4614cf26e8375c60ea7272a5aae
t-akei <145541870+t-akei@users.noreply.github.com>
2023-11-02 05:03:05 +0000

    [課題途中]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    
+    @book = Book.new
+
   end
 
   def edit
@@ -25,21 +26,21 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      
+
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else
       render :edit
     end
   end
-  
-  
+
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
