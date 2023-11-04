0a8a2ff194f973b28b58048dd1ec6e48d538c773
t-akei <145541870+t-akei@users.noreply.github.com>
2023-11-04 08:40:28 +0000

    [課題途中]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,6 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if @user.save
-      flash[:notice] = "You have updated user successfully."
-      redirect_to user_path(@user.id)
-    else
-      render :edit
-    end
   end
 
   def update
