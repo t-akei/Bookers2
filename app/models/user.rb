eb93036d9773017daa4847e4761092afc41960a3
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-01 10:33:27 +0000

    [Start]応用4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id" foreign_key: "followed_id", dependent: :destroy
+  has_many :follow, through: relationships, source: :followed, :follower
 
   has_one_attached :profile_image
 
