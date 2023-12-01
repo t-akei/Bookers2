a271f043339964dc1064e063a18129d80383d75b
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-01 14:41:00 +0000

    [応用4]32

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,6 @@
 class RelationshipsController < ApplicationController
   def create
+    user = User.find(params[:id])
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'relationships/create'
-  get 'relationships/destroy'
   devise_for :users
   root to: 'homes#top'
 
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  end
   get '/home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
