811996a50ce8f006c9a5c1396d9d119cbb12f12f
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-02 10:16:12 +0000

    [応用4]12/-2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
+    get 'follower' => 'relationships#follower', as: 'follower'
+    # フォローをするユーザー＝自分
+    get 'followed' => 'relationships#followed', as: 'followed'
+    # 自分にフォローされるユーザー
   end
   get '/home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
