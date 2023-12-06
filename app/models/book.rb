29186d762d1135131afe29d0fafac3f78beb8b05
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-06 09:58:39 +0000

    [応用5]12/6-2

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,17 @@ class Book < ApplicationRecord
 
   belongs_to :user
   
+  def search_for(content, method)
+    if method == '完全一致'
+      Book.where(name: content)
+    elsif method == '前方一致'
+      Book.where('name LIKE ?', 'content%')
+    elsif method == '後方一致'
+      Book.where('name LIKE ?', '%content')
+    else method == '部分一致'
+      Book.where('name LIKE ?', '%content%')
+    end
+  end
   
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
