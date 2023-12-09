43d5a4154dff420bd3b39d0e848dd7089f00bd9d
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-09 01:48:24 +0000

    [応用6]12/9-1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,6 +8,9 @@ class FavoritesController < ApplicationController
     # 但し.newしているだけで、current_user.favorites.new(book_id: book.id)という
     # このデータ(=インスタンス)はまだデータベースに保存されていない。
     favorite.save
+    # redirect_to request.referer
+    # リダイレクト先を削除、かつJavaScriptリクエストのため
+    # app/views/favorites/create.js.erbファイルを探す
   end
 
   # いいねを外す機能
@@ -15,5 +18,8 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
+    # redirect_to request.referer
+    # リダイレクト先を削除、かつJavaScriptリクエストのため
+    # app/views/favorites/destroy.js.erbファイルを探す
   end
 end
