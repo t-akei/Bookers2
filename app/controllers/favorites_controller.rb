46b5b3cb00f67ec4f4fc8f1f6e55358bf966ecc5
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-09 05:45:54 +0000

    [応用6]12/9-3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,8 @@
 class FavoritesController < ApplicationController
   # いいねをする機能
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     # current_user(今ログインしているユーザー)のfavoritesテーブル(レコード)
     # を.newで新規作成しようとしている。
     # 但し.newしているだけで、current_user.favorites.new(book_id: book.id)という
@@ -15,8 +15,8 @@ class FavoritesController < ApplicationController
 
   # いいねを外す機能
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     # redirect_to request.referer
     # リダイレクト先を削除、かつJavaScriptリクエストのため
