65da2a3a1f25474068b734c035875fdf1335d5f2
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-09 08:25:04 +0000

    [応用6]12/9-4

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to request.referer
+    # redirect_to request.referer
+    # 非同期通信を行う場合は、JavaScriptファイル（.js.erb）を使用して
+    # ビューを更新します。redirect_toを消す事で自動的にアクションに対応するjsファイルを探しにいく。
+    # 今回はcreate.js.erbファイルを探しにいく
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     comment = BookComment.find(params[:id])
     comment.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
   private
