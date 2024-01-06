c236b3a13379101730968f6d80064b75e0067951
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-06 14:02:17 +0000

    [ゲストログイン]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 before_action :is_matching_login_user, only: [:edit, :update]  
+before_action :ensure_guest_user, only: [:edit]
 
   def index
     @users = User.all
@@ -42,4 +43,12 @@ before_action :is_matching_login_user, only: [:edit, :update]
       redirect_to user_path(current_user.id)
     end
   end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.email == "guest@example.com"
+      redirect_to user_path(current_user), notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end
+  
 end
